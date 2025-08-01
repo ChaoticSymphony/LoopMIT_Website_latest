fb2902057067a81d051620c4f41f3c851f8cf739
ChaoticSymphony <93837178+ChaoticSymphony@users.noreply.github.com>
2025-08-01 19:12:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/LoopMIT_Website_latest/', // 👈 ADD THIS LINE
   plugins: [react()],
   server: {
     port: 3000,
